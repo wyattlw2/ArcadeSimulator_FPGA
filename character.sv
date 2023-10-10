44cd57b23bce46da00420bd119c5cc44449bd913
Wyatt Wethington <91353997+wyattlw2@users.noreply.github.com>
2023-10-10 13:26:06 -0500

    Update and rename ball.sv to character.sv

--- character.sv
+++ character.sv
@@ -1,16 +1,4 @@
-//-------------------------------------------------------------------------
-//    Ball.sv                                                            --
-//    Viral Mehta                                                        --
-//    Spring 2005                                                        --
-//                                                                       --
-//    Modified by Stephen Kempf 03-01-2006                               --
-//                              03-12-2007                               --
-//    Translated by Joe Meng    07-07-2013                               --
-//    Fall 2014 Distribution                                             --
-//                                                                       --
-//    For use with ECE 298 Lab 7                                         --
-//    UIUC ECE Department                                                --
-//-------------------------------------------------------------------------
+
 
 
 module  ball ( input Reset, frame_clk,
